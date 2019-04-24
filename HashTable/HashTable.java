fc0a80ca5108d98799329605704de6bd9251cf0f
Richsea <201402377@cs-cnu.org>
2019-04-24 11:13:28 +0900

    finish get and remove hashTable

--- HashTable/HashTable.java
+++ HashTable/HashTable.java
@@ -1,33 +1,95 @@
 public class HashTable implements Map
 {
-    int size = 10;
+    private int size = 10;
     private Entry[] entries = new Entry[size];
     private final Entry NIL = new Entry(null, null);
 
     @Override
     public Object get(Object key) {
+        int hashNum = hash(key);
 
+        while(entries[hashNum] != null)
+        {
+            Entry entry = entries[hashNum];
+
+            if(entry.key .equals(key))
+            {
+                return entry.value;
+            }
+
+            hashNum++;
+            if(hashNum >= entries.length)
+            {
+                hashNum = 0;
+            }
+        }
 
         return null;
     }
 
     @Override
-    public Object put(Object key, Object value) {
+    public Object put(Object key, Object value)
+    {
+        //75%이상 채우면 rehashing 실행
+        int hashNum = hash(key);
+
+        while(entries[hashNum] != null)
+        {
+            Entry entry = entries[hashNum];
+
+            if(entry == NIL)
+            {
+
+            }
+
+            hashNum ++;
+
+            if(hashNum >= entries.length)
+            {
+                hashNum = 0;
+            }
+        }
+
         return null;
     }
 
     @Override
-    public Object remove(Object key) {
+    public Object remove(Object key)
+    {
+        int hashNum = hash(key);
+
+        while(entries[hashNum] != null)
+        {
+            Entry entry = entries[hashNum];
+
+            if(entry.key.equals(key))
+            {
+                Object oldValue = entry.value;
+
+                entries[hashNum] = NIL;
+                size--;
+                return oldValue;
+            }
+
+            hashNum++;
+
+            if(hashNum >= entries.length)
+            {
+                hashNum = 0;
+            }
+        }
+
         return null;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     private int hash(Object key)
     {
+        if(key == null) throw new IllegalArgumentException();   // key 자체가 잘못들어온 경우 예외처리
         return (key.hashCode() & 0x7FFFFFFF) % entries.length;
     }
 
