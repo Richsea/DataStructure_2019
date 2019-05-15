62968c12d16313bf67f96e9230969151dd211653
Richsea <201402377@cs-cnu.org>
2019-05-15 17:13:19 +0900

    finish make 4 order Tree

--- BinaryTree/Main.java
+++ BinaryTree/Main.java
@@ -1,11 +1,39 @@
 public class Main {
     public static void main(String args[])
     {
+        LinkedList<BinaryTree> queue = new LinkedList<BinaryTree>();
+        int data = 0;
+        BinaryTree root = new BinaryTree(data);
+        BinaryTree tree;
+        queue.addLast(root);
+        tree = queue.poll();
 
-    }
+        while(data != 10)
+        {
+            data ++;
 
-    public void levelOrderPrint()
-    {
+            if(data %2 == 0)
+            {
+                tree.setRight(new BinaryTree(data));
+                queue.addLast(tree.getRight());
+                tree = queue.poll();
+            }
+            else
+            {
+                tree.setLeft(new BinaryTree(data));
+                queue.addLast(tree.getLeft());
+            }
+        }
+
+        PrintTree print = new PrintTree(root);
+        System.out.println("[level order print]");
+        print.levelOrderPrint();
+        System.out.println("\n\n[pre order print]");
+        print.preOrderPrint(root);
+        System.out.println("\n\n[post order print]");
+        print.postOrderPrint(root);
+        System.out.println("\n\n[in order print]");
+        print.inOrderPrint(root);
 
     }
 }
--- BinaryTree/PrintTree.java
+++ BinaryTree/PrintTree.java
@@ -1,14 +1,16 @@
 public class PrintTree {
     private BinaryTree root;
+    LinkedList<Object> printList;
 
     public PrintTree(BinaryTree root)
     {
         this.root = root;
     }
+
     public void levelOrderPrint()
     {
         LinkedList<BinaryTree> queue = new LinkedList<BinaryTree>();
-        LinkedList<Object> printList = new LinkedList<Object>();
+        StringBuffer sb = new StringBuffer();
         BinaryTree subtrees;
 
         if(root == null) return;
@@ -21,19 +23,68 @@ public class PrintTree {
             if(subtrees.getLeft() != null) queue.addLast(subtrees.getLeft());
             if(subtrees.getRight() != null) queue.addLast(subtrees.getRight());
 
-            printList.addLast(subtrees);
+            sb.append(subtrees.getData() + " -> ");
+            //printList.addLast(subtrees.getData());
         }
-        printConsole(printList);
+        sb.delete(sb.length()-3, sb.length());
+        System.out.print(sb);
     }
 
-    public void printConsole(LinkedList<Object> list)
+    /*
+    전위 순서 순회
+     */
+    public void preOrderPrint(BinaryTree root)
     {
-        if(list.size() == 0)    return;
-        System.out.println(list.get(0));
-        for(int i = 1; i < list.size(); i++)
+        if(root == null) return;
+        System.out.print(root.getData());
+        if(root.getLeft() != null)
         {
-            System.out.println(" -> ");
-            System.out.print(list.get(i));
+
+            System.out.print(" -> ");
+            preOrderPrint(root.getLeft());
+        }
+        if(root.getRight() != null)
+        {
+            System.out.print(" -> ");
+            preOrderPrint(root.getRight());
+        }
+    }
+
+    /*
+    후위 순서 순회
+     */
+    public void postOrderPrint(BinaryTree root)
+    {
+        if(root == null) return;
+        if(root.getLeft() != null)
+        {
+            postOrderPrint(root.getLeft());
+            System.out.print(" -> ");
+        }
+        if(root.getRight() != null)
+        {
+            postOrderPrint(root.getRight());
+            System.out.print(" -> ");
+        }
+        System.out.print(root.getData());
+    }
+
+    /*
+    중위 순서 순회
+     */
+    public void inOrderPrint(BinaryTree root)
+    {
+        if(root == null) return;
+        if(root.getLeft() != null)
+        {
+            inOrderPrint(root.getLeft());
+            System.out.print(" -> ");
+        }
+        System.out.print(root.getData());
+        if(root.getRight() != null)
+        {
+            System.out.print(" -> ");
+            inOrderPrint(root.getRight());
         }
     }
 }
